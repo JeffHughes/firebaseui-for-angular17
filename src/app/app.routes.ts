c4f74c9d642b4b71c9a1c1dacbdfd7fb77e5744e
Jeff Hughes <me@jeffhughes.com>
2024-04-09 14:34:29 -0500

    added home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
  
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
  
 
 export const routes: Routes = [  
- 
+     { path: 'home', component: HomeComponent },
     {
         path: 'auth', loadChildren: () => import('./auth/auth.module')
             .then(m => m.AuthModule)
